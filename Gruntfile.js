8d60906ce5dcbb667ac5baedbbc7b267be4145a1
Rodriguez <764092@cognizant.com>
2019-06-26 10:46:06 +0200

    fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -124,6 +124,7 @@ module.exports = (grunt) => {
         // git merge --no-ff release/1.2.0
         grunt.config.set('cnf.branchName', `release/${grunt.config('pkg.version')}`);
         grunt.task.run('gitmerge');
+        grunt.task.run('gitpush');
         // git tag -a 1.2.0
         grunt.config.set('cnf.tag', `${grunt.config('pkg.version')}`);
         grunt.config.set('cnf.commitMessage', `Release Tag ${grunt.config('pkg.version')}`);
@@ -134,6 +135,7 @@ module.exports = (grunt) => {
         // git merge --no-ff release/1.2.0
         grunt.config.set('cnf.branchName', `release/${grunt.config('pkg.version')}`);
         grunt.task.run('gitmerge');
+        grunt.task.run('gitpush');
         //TODO: delete release branch 
     });
 
